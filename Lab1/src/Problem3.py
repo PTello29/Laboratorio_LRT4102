8109502c3d008447bd447d09d0eaf5f533810829
PTello29 <pablo.tellova@udlap.mx>
2025-02-23 21:33:42 -0600

    Problema 3

--- Lab1/src/Problem3.py
+++ Lab1/src/Problem3.py
@@ -1,11 +1,18 @@
 # Problema 3: Crea una lista de nombre + sueldo por hora + horas trabajadas de 
 # al menos seis operadores. Imprime el nombre y el sueldo a pagar de cada operador.
 
-preguntar = int(input("¿Cuántos operadores deseas agregar? "))
+# Lista de operadores
+operadores = [
+    ["Juan", 10, 40],
+    ["María", 12, 35],
+    ["Pedro", 8, 45],
+    ["Ana", 15, 30],
+    ["Luis", 20, 20],
+    ["Sofía", 25, 15]
+]   
 
-for i in range(preguntar):
-    valores = input("Introduce el nombre del operador, horas trabajadas y sueldo por hora: ")
-    lista = valores.split()
-
-print(lista)
-    
+# Imprimir nombre y sueldo a pagar de cada operador
+for operador in operadores:
+    nombre = operador[0]
+    sueldo = operador[1] * operador[2]
+    print(f"{nombre} debe recibir ${sueldo} por su trabajo")
